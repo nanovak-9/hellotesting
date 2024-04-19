0f74929432b13a3fa5a5806800e51e8311275360
Ana Gomez <anaa.gmz@gmail.com>
2024-04-19 10:52:41 -0600

    Update cloudbuild file and text change test

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ dotenv.config()
 
 app.get('/', (req, res) => {
   const name = process.env.NAME || 'World'
-  res.send(`YES! ${name} branch~`)
+  res.send(`Hello, ${name} branch~`)
 })
 
 const port = parseInt(process.env.PORT) || 8080
