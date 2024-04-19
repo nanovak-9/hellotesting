b632d7b84a752ab0d08559ceb11a76d99fe35659
Ana Gomez <anaa.gmz@gmail.com>
2024-04-19 11:24:58 -0600

    Text change

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ dotenv.config()
 
 app.get('/', (req, res) => {
   const name = process.env.NAME || 'World'
-  res.send(`READTHEROOM, BROOOO ${name}! Service is up and running!`)
+  res.send(`${name}! Service is up and running!`)
 })
 
 const port = parseInt(process.env.PORT) || 8080
